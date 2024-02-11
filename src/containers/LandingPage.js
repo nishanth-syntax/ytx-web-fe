e4896f30cd8034cbcae83e47bf4e7a0281b3d5bd
Nishanth Saka <135235013+nishanth-syntax@users.noreply.github.com>
2024-02-11 13:20:36 +0530

    local changes

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {
     Card,
     Container,
+    CardContent,
     Image,
     List,
 } from 'semantic-ui-react'
@@ -24,8 +25,34 @@ const LandingPage = () => {
     }
 
     return (
-        <Container style={{ padding: 5, margin: 5 }}>
-            <List horizontal style={{ height: '70vh', overflowY: 'scroll' }}>
+        <Container>
+            <List horizontal style={{
+                display: 'flex',
+                flex: 1,
+                height: '15vh',
+                overflowX: 'scroll'
+            }}>
+                {sampleDataArray.map((landingPageData, index) => (
+                    <Container style={{
+                        flex: 1,
+                        verticalAlign: 'middle',
+                        width: '50%',
+                        padding: 5,
+                        margin: 5
+                    }}>
+                        <Card
+                            style={{ width: '120px' }}
+                            key={`main-id-${index}`}
+                            onClick={handleClick}
+                        >
+                            <Image
+                                key={`image-id-${index}`}
+                                src={"https://cdn.dribbble.com/users/1229964/screenshots/15564939/media/e326e8be96f692b4fe0a6940e6b1f45f.jpg?resize=1600x1200&vertical=center"} wrapped ui={false} />
+                        </Card>
+                    </Container>
+                ))}
+            </List>
+            <List horizontal style={{ width: '100%', height: '70vh', overflowY: 'scroll' }}>
                 {sampleDataArray.map((landingPageData, index) => (
                     <Card
                         style={{ width: '100%' }}
@@ -34,19 +61,6 @@ const LandingPage = () => {
                     >
                         <Image key={`image-id-${index}`} src={landingPageData.imageURL} wrapped ui={false} />
                     </Card>
-                ))}
-            </List>
-            <List horizontal style={{ display: 'flex', flex: 1, height: '100%', overflowX: 'scroll' }}>
-                {sampleDataArray.map((landingPageData, index) => (
-                    <Container style={{flex: 1, verticalAlign: 'middle'}}>
-                        <Card
-                            style={{ height: '100%' }}
-                            key={`main-id-${index}`}
-                            onClick={handleClick}
-                        >                            
-                            <Image key={`image-id-${index}`} src={landingPageData.imageURL} wrapped ui={false} />
-                        </Card>
-                    </Container>
                 ))}
             </List>
         </Container>
